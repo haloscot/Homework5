75b0a7b91b4f4702a1de661dcbe9d3283cf77277
spikeymikey73 <alfordmichael19@gmail.com>
2019-11-15 12:39:39 +1100

    updating time class colour schemes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,56 +1,68 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-    var date = moment().format('DD MMM YYYY');
-    $('#today').text(moment().format('DD MMM YYYY'));
+    //push date to bottom of jumbotron
+    var now = moment().format('DD MMM YYYY');
+    $('#currentDay').text(now);
 
-    var now = moment().get('hour');
-    console.log(now);
+    //get the hour of day to set colours for hour element.
+    var hourEl = moment().get('hour');
+
+    var test = $(".hour");
+
+    // change colour of time span based on time of day.
+    $.each(test, function() {
+        if ($( this ).attr('value') > hourEl) {
+            test.addClass( "future" );
+            console.log(test);
+        }
+        else if ($( this ).attr('value') === hourEl) {
+            test.addClass( "present" );
+        }
+        else {
+            test.addClass( "past" );
+        }
+    });
 
 
+    //Event listener for all buttons
+    $('#saveBtn').click(function(event) {
+        var valueAttr = ("data-hour").val();
+        var notes = JSON.parse(window.localStorage.getItem("#description"));
+        console.log('clicked', $(this.text()));
+        alert("Save button clicked!");
+    });
 
-    // var table = $('.table').tableToJSON();
-    // console.log(table);
-
-    var content = $('.content')
-    console.log(content);
-
-    content.text("this is a test! Yo!");
-
-
-    $(document).on('click', '.content', function(event)
-{
-	event.preventDefault();
-
-	if($(this).attr('edit_type') == 'button')
-	{
-		return false;
-	}
-
-	//make div editable
-	$(this).closest('div').attr('contenteditable', 'true');
-	//add bg css
-	$(this).addClass('bg-warning');
-
-	$(this).focus();
-});
-
-$(document).on('blur', '.content', function(event) {
-    $(this).closest('div').attr('contenteditable', 'false');
-    $(this).removeClass('bg-warning');
-    // $(this).unfocus();
-
+    $("#saveBtn").click(function() {
+        var value = $("#saveBtn").index(this)
+        console.log(value);
+    });
 });
 
 
+// additional functions:
 
+// Copy text from text area to clipboard
 
-    //button click save event
-    // $( "#saveBtn" ).click(function() {
-    //     $( this ).click(function() {
-    //         console.log("that was clicked!");
-    //     });
-    //   });
-});
+// function copyToClipboard() {
+//   var passwordText = document.querySelector("#password");
 
+//   passwordText.select();
+//   document.execCommand("copy");
 
+//   alert(
+//     "Your password " + passwordText.value + " was copied to your clipboard."
+//   );
+// }
+
+// local storage set/get
+
+// function printHighscores() {
+//   // either get scores from localstorage or set to empty array
+//   var notes = JSON.parse(window.localStorage.getItem("#description")) || [];
+
+//   // sort highscores by score property in descending order
+//   notes.sort(function(a, b) {
+//     return b.score - a.score;
+//   });
+// }
 
