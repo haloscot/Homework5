ab4761854efc76cc93786ee4088e7e53b208fda2
spikeymikey73 <alfordmichael19@gmail.com>
2019-11-16 21:53:46 +1100

    working on click event to save textarea contents

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,46 +1,56 @@
 $(document).ready(function() {
 
+    var notes = [];
+
     //push date to bottom of jumbotron
     var now = moment().format('DD MMM YYYY');
     $('#currentDay').text(now);
 
     //get the hour of day to set colours for hour element.
     var hourEl = moment().get('hour');
-    console.log(hourEl);
 
-    // change colour of time span based on time of day.
+    // change colour of each time span element based on (value of) time of day.
     $(".hour").each(function() {
         var timeIndex = parseInt($(this).attr('value'));
         if (timeIndex < hourEl){
             $(this).addClass( "past" );
-            console.log(this + " past");
         }
         if (timeIndex === hourEl) {
             $(this).addClass( "present" );
-            console.log(this);
         }
         if (timeIndex > hourEl) {
             $(this).addClass( "future" );
-            console.log(this);
-            console.log("future");
         }
     });
 
-
     // //Event listener for all buttons
-    // $('#saveBtn').click(function(event) {
+    // $('.saveBtn').click(function(event) {
     //     var valueAttr = ("data-hour").val();
     //     var notes = JSON.parse(window.localStorage.getItem("#description"));
     //     console.log('clicked', $(this.text()));
     //     alert("Save button clicked!");
     // });
 
-    // $("#saveBtn").click(function() {
-    //     var value = $("#saveBtn").index(this)
-    //     console.log(value);
-    // });
-});
+    function copyText() {
+        var copied = $("#description");
 
+        copied.select();
+        document.execCommand("copy");
+
+        return copied;
+
+        alert("Your password " + passwordText.value + " was copied to your clipboard.");
+      }
+
+    $(".saveBtn").on("click", function() {
+        //notes = copyText();
+        //Retrieve contents of textarea and save to
+        localStorage.setItem("notes", $('#description').text());
+        console.log($('#description').text());
+        //JSON.stringify(notes)
+
+    });
+});
 
 // additional functions:
 
